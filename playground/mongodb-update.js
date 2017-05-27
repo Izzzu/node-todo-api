2c2b993700d9523d8f4576666dde534c397ab68c
Izabela Kułakowska <izabela@Izabelas-iMac.local>
2017-05-27 16:46:18 +0200

    Update script

--- playground/mongodb-update.js
+++ playground/mongodb-update.js
@@ -10,11 +10,34 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   console.log('Connected to mogodb server');
 
 
-  db.collection('Todos').findOneAndDelete({completed: false}).then((result) => {
+  db.collection('Todos').findOneAndUpdate({
+      _id: new ObjectID("59298a43a115a6bfc32afdd5")
+    }, {
+      $set: {
+        completed: true
+      }
+    }, {
+      returnOriginal: false
+    }).then((result) => {
     console.log(result);
   })
 
-  //to samo z users
+  //change name w users + increment age w users
+
+  db.collection('Users').findOneAndUpdate({
+      _id: new ObjectID("5927581c571ecfb8c8ca40ba")
+    }, {
+      $set: {
+        name: 'Stefan'
+      },
+      $inc: {
+        age: 1
+      }
+    }, {
+      returnOriginal: false
+    }).then((result) => {
+    console.log(result);
+  })
 
 
   db.close();
